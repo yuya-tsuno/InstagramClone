11ef2e8fcde743fa55e6a78e6f2c2f0a024209bd
Tsuno Yuya <2ch.yuya@gmail.com>
2020-01-08 21:52:21 +0900

    確認画面の追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -61,6 +61,10 @@ class FeedsController < ApplicationController
     end
   end
 
+  def confirm
+    @feed = Feed.new(feed_params)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_feed
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :feeds
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
 end
