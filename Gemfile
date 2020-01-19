c2d2be67716defea6a64e49632fbe38b99465bf9
Tsuno Yuya <2ch.yuya@gmail.com>
2020-01-19 19:37:15 +0900

    シードデータの追加

--- Gemfile
+++ Gemfile
@@ -38,3 +38,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'bcrypt', '3.1.11'
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do |n|
+50.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "password"
+  image = Faker::Name.name
   User.create!(name: name,
                email: email,
                password: password,
                password_confirmation: password,
+               image: image,
                )
 end
