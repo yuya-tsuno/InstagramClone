21f3bdd7ef676ec43abef326a63c571be3869123
Tsuno Yuya <2ch.yuya@gmail.com>
2020-01-19 19:22:41 +0900

    辞書ファイルで日本語化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
+
+  # falseに変更でエラーメッセージを変更
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
